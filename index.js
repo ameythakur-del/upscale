4014ee5e77211b505e1a97e234aed6f5299d4534
Amey Thakur <amey36thakur@gmail.com>
2020-08-15 11:28:02 +0530

    d

--- index.js
+++ index.js
@@ -2,7 +2,21 @@ function myFunction() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
     x.className += " responsive";
+     $("myTopnav").click(function () {
+        windowsize = $(window).width();
+
+        if (windowsize < 440) {
+            $(".navbar-header").css({"background-color": "#ffffff", "-webkit-box-shadow": "0px 1px 4px 0px rgba(180, 180, 180, 0.5)", "-moz-box-shadow": "0px 1px 4px 0px rgba(180,180,180, 0.5)", "box-shadow": "0px 1px 4px 0px rgba(180,180,180, 0.5)"});
+        }
+    });
   } else {
     x.className = "topnav";
+     $("myTopnav").click(function () {
+        windowsize = $(window).width();
+
+        if (windowsize < 440) {
+            $(".navbar-header").css({"background-color": "#ffffff", "-webkit-box-shadow": "0px 1px 4px 0px rgba(180, 180, 180, 0.5)", "-moz-box-shadow": "0px 1px 4px 0px rgba(180,180,180, 0.5)", "box-shadow": "0px 1px 4px 0px rgba(180,180,180, 0.5)"});
+        }
+    });
   }
 } 
